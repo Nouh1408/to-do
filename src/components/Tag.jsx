dfe23af791375a68f99cd64fedbc187fb63b504b
Nouh1408 <ahmedinouh@gmail.com>
2025-08-01 18:48:19 +0300

    i don't know what to write here

---
+++ src/components/Tag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Tag.css'
+export default function Tag() {
+  return (
+    <div>
+      <button className="tag">HTML</button>
+    </div>
+  )
+}
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import './Task.css'; 
+import "./Task.css";
+import Tag from "./Tag.jsx";
 export default function Task() {
   return (
     <div>
       <header className="header">
-        <form >
+        <form>
           <input type="text" className="U-Input" placeholder="Enter Task" />
           <div>
             <div className="task-form">
-             <div>
-               <button className="tag">HTML</button>
-              <button className="tag">CSS</button>
-              <button className="tag">JAVASCRIPT</button>
-              <button className="tag">REACT</button>
-             </div>
+              <div>
+                <Tag />
+                <Tag />
+                <Tag />
+                <Tag />
+              </div>
 
               <div>
                 <select className="status">
-                <option value="todo">To do</option>
-                <option value="doing">Doing</option>
-                <option value="done">Done</option>
-              </select>
-                <button type="submit" className="add-task">Add Task</button>
+                  <option value="todo">To do</option>
+                  <option value="doing">Doing</option>
+                  <option value="done">Done</option>
+                </select>
+                <button type="submit" className="add-task">
+                  Add Task
+                </button>
               </div>
             </div>
           </div>
