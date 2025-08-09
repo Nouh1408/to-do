0f78c466323201f7afd1f2ac09b8886d493b681a
Nouh1408 <ahmedinouh@gmail.com>
2025-08-09 05:54:04 +0300

    continue later

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TaskCard() {
+  return (
+    <div>
+      <div>
+        <p>sample text</p>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function TaskColumn(props) {
+export default function TaskColumn({icon,title}) {
+    
   return (
     <div>
       <h2 className="d-flex">
-        {" "}
-        <img className="w-25 ms-2" src={Todo} alt="" />{props.title}
+        <img className="w-25 ms-2" src={icon} alt="" />{title}
       </h2>
     </div>
   );
